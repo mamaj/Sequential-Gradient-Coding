410ca64bd95f695bf708047e6a45d7cc70b720cb
mamaj@RezaDesktop <mamareza.e@gmail.com>
2022-04-21 22:53:09 -0400

    test mock delay profile

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
 import numpy as np
 from multiplexed_sgc import Multiplexed_SGC
 
+delays = np.random.normal(size=(4, 6))
+delays += 10
+delays = np.abs(delays)
+
 
 model = Multiplexed_SGC(n=4,
                 B=2,
                 W=3, 
                 lambd=2,
                 rounds=6, 
-                mu=1,
-                delays=np.ones((4, 6)))
+                mu=.2,
+                delays=delays)
 
 
 model.run()
+model.state
