58ccb3109c4f8ef0ba5fa918ab9748c1b77df013
mamaj@RezaDesktop <mamareza.e@gmail.com>
2022-05-09 20:03:37 -0400

    cleaning

--- test.py
+++ test.py
@@ -1,45 +1,56 @@
 import numpy as np
-from multiplexed_sgc import MultiplexedSGC
-from gradient_coding import GradientCoding
-from selective_repeat_sgc import SelectiveRepeatSGC
-from utils import load_windows_exp, get_durations
 
-load_windows_exp(
+from gradient_coding import GradientCoding
+from multiplexed_sgc import MultiplexedSGC
+from selective_repeat_sgc import SelectiveRepeatSGC
+from utils import get_durations, load_windows_exp
+
+# params
+args = dict(
+    n = 100,
+    rounds = 80,
+    mu = 0.1,
+)
+
+models = [
+    (GradientCoding, {
+        's': 2
+        }),
+    (SelectiveRepeatSGC, {
+        'B': 2,
+        'W': 3,
+        'lambd': 2
+        }),
+    (MultiplexedSGC, {
+        'B': 2,
+        'W': 3,
+        'lambd': 2
+        }),
+]
+
+# load a delay profile
+rounds = load_windows_exp(
     nworkers=100,
     ninvokes=100,
     size=1000,
     region='Tokyo',
     folder='../aws-lambda/exp_long_3',
 )
+delays = get_durations(rounds)
+delays = delays.T # (workers, rounds)
+delays = delays[:args['n'], :]
+print(delays.shape)
 
-delays = np.random.normal(size=(4, 6))
-delays += 10
-delays = np.abs(delays)
-
-
-model = MultiplexedSGC(n=4,
-                        B=2,
-                        W=3,
-                        lambd=2,
-                        rounds=6,
-                        mu=.1,
-                        delays=delays)
-
-model = GradientCoding(n=4,
-                       s=2,
-                       rounds=6,
-                       mu=.1,
-                       delays=delays)
+# random delay profile  
+# delays = np.random.normal(size=(4, 6))
+# delays += 10
+# delays = np.abs(delays)
 
 
-model = SelectiveRepeatSGC(n=4,
-                           B=2,
-                           W=3,
-                           lambd=2,
-                           rounds=6,
-                           mu=.1,
-                           delays=delays)
-
-model.run()
-print(model.durations)
-print(model.state)
+for Model, params in models:
+    model = Model(**args, **params, delays=delays)
+    model.run()
+    print(model.__class__.__name__)
+    print(f'runtime: {model.durations.sum():.2f}')
+    print(f'load: {model.load}')
+    # print(model.state)
